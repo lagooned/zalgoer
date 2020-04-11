5b451104f6485d479af8bb258c3d9ba7ffd57151
Jared Engler <jared.lite@gmail.com>
2020-04-10 20:42:26 -0500

    change port from 3000 -> 5000

--- server.js
+++ server.js
@@ -98,8 +98,8 @@ function zalgo(text) {
 
 }
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
+app.listen(5000, () => {
+    console.log("Server running on port 5000")
 })
 
 app.get("/", (req, res, next) => {
